80eaae1e96cf152bf7ba6b1292723b554a386a48
Jack Fraser <jackcfraser@hotmail.com>
2021-08-21 14:46:33 +1000

    Welcome page work

--- govhack/src/App.js
+++ govhack/src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import Header from './Header/HeaderBar';
-// import { createMuiTheme } from '@material-ui/core/styles';
-// import { ThemeProvider } from '@material-ui/styles';
-// import 'typeface-roboto';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import 'typeface-roboto';
 import './App.css';
 
-// const theme = createMuiTheme({
-//   typography: {
-//     htmlFontSize: 18,
-//     fontFamily: "Roboto",
-//   },
-// });
+const theme = createMuiTheme({
+  typography: {
+    htmlFontSize: 18,
+    fontFamily: "Roboto",
+  },
+});
 
 function App() {
   return (
-    // <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
         <div id="root">
           <Header />
         </div>
-      // </ThemeProvider>
+    </ThemeProvider>
   );
 }
 
